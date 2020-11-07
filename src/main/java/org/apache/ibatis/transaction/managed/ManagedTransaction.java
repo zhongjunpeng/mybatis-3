93883620181a8da8c6fcc3d7f891873d363baa99
zhongjp <1225930063.com>
2020-11-07 14:31:36 +0800

    事务模块分析

--- src/main/java/org/apache/ibatis/transaction/managed/ManagedTransaction.java
+++ src/main/java/org/apache/ibatis/transaction/managed/ManagedTransaction.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.transaction.managed;
 
@@ -31,70 +31,84 @@ import org.apache.ibatis.transaction.Transaction;
  * By default, it closes the connection but can be configured not to do it.
  *
  * @author Clinton Begin
- *
  * @see ManagedTransactionFactory
  */
+// 基于容器管理的事务实现类。
 public class ManagedTransaction implements Transaction {
 
-  private static final Log log = LogFactory.getLog(ManagedTransaction.class);
+    private static final Log log = LogFactory.getLog(ManagedTransaction.class);
 
-  private DataSource dataSource;
-  private TransactionIsolationLevel level;
-  private Connection connection;
-  private final boolean closeConnection;
+    /**
+     * DataSource 对象
+     */
+    private DataSource dataSource;
+    /**
+     * 事务隔离级别
+     */
+    private TransactionIsolationLevel level;
+    /**
+     * Connection 对象
+     */
+    private Connection connection;
+    /**
+     * 是否关闭连接
+     *
+     * 这个属性是和 {@link org.apache.ibatis.transaction.jdbc.JdbcTransaction} 不同的
+     */
+    private final boolean closeConnection;
 
-  public ManagedTransaction(Connection connection, boolean closeConnection) {
-    this.connection = connection;
-    this.closeConnection = closeConnection;
-  }
-
-  public ManagedTransaction(DataSource ds, TransactionIsolationLevel level, boolean closeConnection) {
-    this.dataSource = ds;
-    this.level = level;
-    this.closeConnection = closeConnection;
-  }
-
-  @Override
-  public Connection getConnection() throws SQLException {
-    if (this.connection == null) {
-      openConnection();
+    public ManagedTransaction(Connection connection, boolean closeConnection) {
+        this.connection = connection;
+        this.closeConnection = closeConnection;
     }
-    return this.connection;
-  }
 
-  @Override
-  public void commit() throws SQLException {
-    // Does nothing
-  }
-
-  @Override
-  public void rollback() throws SQLException {
-    // Does nothing
-  }
-
-  @Override
-  public void close() throws SQLException {
-    if (this.closeConnection && this.connection != null) {
-      if (log.isDebugEnabled()) {
-        log.debug("Closing JDBC Connection [" + this.connection + "]");
-      }
-      this.connection.close();
+    public ManagedTransaction(DataSource ds, TransactionIsolationLevel level, boolean closeConnection) {
+        this.dataSource = ds;
+        this.level = level;
+        this.closeConnection = closeConnection;
     }
-  }
 
-  protected void openConnection() throws SQLException {
-    if (log.isDebugEnabled()) {
-      log.debug("Opening JDBC Connection");
+    @Override
+    public Connection getConnection() throws SQLException {
+        if (this.connection == null) {
+            openConnection();
+        }
+        return this.connection;
     }
-    this.connection = this.dataSource.getConnection();
-    if (this.level != null) {
-      this.connection.setTransactionIsolation(this.level.getLevel());
-    }
-  }
 
-  @Override
-  public Integer getTimeout() throws SQLException {
-    return null;
-  }
+    @Override
+    public void commit() throws SQLException {
+        // Does nothing
+    }
+
+    @Override
+    public void rollback() throws SQLException {
+        // Does nothing
+    }
+
+    @Override
+    public void close() throws SQLException {
+        if (this.closeConnection && this.connection != null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Closing JDBC Connection [" + this.connection + "]");
+            }
+            this.connection.close();
+        }
+    }
+
+    protected void openConnection() throws SQLException {
+        if (log.isDebugEnabled()) {
+            log.debug("Opening JDBC Connection");
+        }
+        this.connection = this.dataSource.getConnection();
+        if (this.level != null) {
+            this.connection.setTransactionIsolation(this.level.getLevel());
+        }
+    }
+
+    @Override
+    public Integer getTimeout() throws SQLException {
+        return null;
+    }
 
 }
